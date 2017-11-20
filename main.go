88b93989917e081281bf18ec5562ac55f88ebc81
Junyu <zhujunyu168@hotmail.com>
2017-11-20 14:58:59 +1300

    hello t2

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func main() {
 	log.Println("Starting pipeline application...")
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello t1\n")
+		fmt.Fprintf(w, "Hello t2\n")
 	})
 
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
